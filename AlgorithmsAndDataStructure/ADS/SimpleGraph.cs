93519b732472339d2ef8db5e603694cbe2149534
Julia Mishlanova <112238047+Julia-Mishlanova@users.noreply.github.com>
2025-04-23 17:06:12 +0400

    Update SimpleGraph.cs

--- AlgorithmsAndDataStructure/ADS/SimpleGraph.cs
+++ AlgorithmsAndDataStructure/ADS/SimpleGraph.cs
@@ -9,10 +9,11 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp_ADS
 {
-    public class Vertex
+   public class Vertex
     {
-        public bool Hit;
         public int Value;
+        public bool Hit;
+
         public Vertex(int val)
         {
             Value = val;
@@ -32,131 +33,6 @@ namespace ConsoleApp_ADS
             m_adjacency = new int[size, size];
             vertex = new Vertex[size];
         }
-
-        public void AddVertex(int value)
-        {
-
-            int i = 0;
-            bool hasPlace = false;
-
-            while (i < max_vertex)
-            {
-                if (vertex[i] == null)
-                {
-                    hasPlace = true;
-                    break;
-                }
-                i++;
-            }
-            if (hasPlace)
-            {
-                vertex[i] = new Vertex(value);
-            }
-        }
-
-        // v - индекс вершины в массиве vertex
-        public void RemoveVertex(int v)
-        {
-            vertex = GetNewVertex(v);
-            m_adjacency = GetNewAdjacency(v);
-
-            max_vertex--;
-        }
-
-        private int[,] GetNewAdjacency(int v)
-        {
-            var adjacency = new int[max_vertex - 1, max_vertex - 1];
-
-            for (int i = 0, g = 0; i < max_vertex && g < max_vertex - 1; i++)
-            {
-                if (i != v)
-                {
-                    for (int j = 0, q = 0; j < max_vertex && q < max_vertex - 1; j++)
-                    {
-                        if (j != v)
-                        {
-                            adjacency[g, q] = m_adjacency[i, j];
-                            q++;
-                        }
-                    }
-                    g++;
-                }
-            }
-
-            return adjacency;
-        }
-
-        private Vertex[] GetNewVertex(int v)
-        {
-            var newVertex = new Vertex[max_vertex - 1];
-
-            for (int i = 0, j = 0; i < max_vertex && j < max_vertex - 1; i++)
-            {
-                if (i != v)
-                {
-                    if (vertex[i] == null) return null;
-
-                    var value = vertex[i].Value;
-                    newVertex[j] = new Vertex(value);
-                    j++;
-                }
-            }
-
-            return newVertex;
-        }
-
-        public bool IsEdge(int v1, int v2)
-        {
-
-            var indV1 = IndexOf(v1);
-            var indV2 = IndexOf(v2);
-
-            if (indV1 == -1 || indV2 == -1) return false; 
-
-            if (m_adjacency[indV1, indV2] == 1 || m_adjacency[indV2, indV1] == 1)
-            {
-                return true;
-            }
-
-            return false;
-        }
-
-        public void AddEdge(int v1, int v2)
-        {
-
-            var indV1 = IndexOf(v1);
-            var indV2 = IndexOf(v2);
-
-            if (indV1 == -1 || indV2 == -1) return; 
-
-            m_adjacency[indV1, indV2] = 1;
-            m_adjacency[indV2, indV1] = 1;
-        }
-
-        private int IndexOf(int v1)
-        {
-            for (int i = 0; i < max_vertex; i++)
-            {
-                if (vertex[i] == null) continue;
-
-                if (Equals(vertex[i].Value, v1))
-                {
-                    return i;
-                }
-            }
-            return -1;
-        }
-        public void RemoveEdge(int v1, int v2)
-        {
-            var indV1 = IndexOf(v1);
-            var indV2 = IndexOf(v2);
-
-            if (indV1 == -1 || indV2 == -1) return;
-
-            m_adjacency[indV1, indV2] = 0;
-            m_adjacency[indV2, indV1] = 0;
-        }
-
         public List<Vertex> DepthFirstSearch(int VFrom, int VTo)
         {
             foreach (var v in vertex)
@@ -254,6 +130,51 @@ namespace ConsoleApp_ADS
             return route;
         }
 
+        public void AddVertex(int value)
+        {
+            for (int i = 0; i < max_vertex; i++)
+            {
+                if (vertex[i] == null)
+                {
+                    vertex[i] = new Vertex(value);
+                    break;
+                }
+            }
+        }
+
+        // Здесь и далее, параметры v - индекс вершины в списке
+        public void RemoveVertex(int v)
+        {
+            vertex[v] = null;
+
+            for (int i = 0; i < max_vertex; i++)
+            {
+                m_adjacency[v, i] = 0;
+                m_adjacency[i, v] = 0;
+            }
+        }
+
+        public bool IsEdge(int v1, int v2)
+        {
+            if (m_adjacency[v1, v2] == 1 && m_adjacency[v2, v1] == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void AddEdge(int v1, int v2)
+        {
+            m_adjacency[v1, v2] = 1;
+            m_adjacency[v2, v1] = 1;
+        }
+
+        public void RemoveEdge(int v1, int v2)
+        {
+            m_adjacency[v1, v2] = 0;
+            m_adjacency[v2, v1] = 0;
+        }
+
         public List<Vertex> WeakVertices()
         {
             List<Vertex> vertices = new List<Vertex>();
@@ -290,6 +211,4 @@ namespace ConsoleApp_ADS
         }
 
     }
-
-
 }
